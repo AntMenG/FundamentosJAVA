6ebf38f213d59cde393fc28c4c748ff5bbe9aef4
Antonio Mendoza <antonio.mendoza.mmg@gmail.com>
2019-07-28 19:09:26 -0500

    init

---
+++ Proyecto.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Proyecto {
+    public static void main (String args[]) {
+        /*
+            byte	8 bits	   -128	                   127
+            short	16 bits	   -32768	               32767
+            int	    32 bits	   -2147483648	           2147483647
+            long	64 bits	   -9223372036854775808	   9223372036854775807
+            float	32 bits	   -3.402823e38	           3.402823e38
+            double	64 bits	   -1.79769313486232e308   1.79769313486232e308
+            char	16 bits	   '\u0000'	               '\uffff'
+        */
+        byte edad = 100;
+        System.out.println(edad);
+    }
+}
